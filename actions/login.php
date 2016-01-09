ab27e018ea16edd8b6a9c06406c22bc6deda76c3
William Funk <contact@WilliamRADFunk.com>
2016-01-08 19:51:34 -0500

    Forget to add one file for previous commit.

---
+++ actions/login.php
@@ -0,0 +1,43 @@
+<?php
+header("content-type:application/json");
+include_once "config.php";
+
+$input = file_get_contents('php://input');
+$object = json_encode($input, JSON_FORCE_OBJECT);
+
+$inputArray = explode(",", $input);
+$name = substr($inputArray[0], 9, strlen($inputArray[0])-10);
+$pwd = substr($inputArray[1], 7, strlen($inputArray[1])-9);
+
+// Create connection
+$conn = new mysqli($hostname, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error)
+{
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Getting password attached to username.
+$sql = 'SELECT Password FROM Login WHERE Username="' . $name . '"';
+$result = $conn->query($sql);
+$pwd_db = "";
+while ( $db_row = $result->fetch_array(MYSQLI_ASSOC) )
+{
+	$pwd_db = $db_row['Password'];
+}
+$conn->close();
+
+//Now compare input password with password in database
+if( ($pwd_db == "") || ($pwd_db != $pwd) )
+{
+	print "Nope!";
+	return "Invalid";
+}
+else
+{
+	session_start();
+	$_SESSION["key"] = $key;
+	print "You got it!";
+	return "Valid";
+}
+?>
